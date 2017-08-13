8cf868e83b8dba1a8a1e41c47138df29c58d690d
mplibunao-shore <mark.libunao@shoresuite.com>
2017-08-13 13:13:38 +0800

    Added exercise 13

---
+++ ex13.rb
@@ -0,0 +1,8 @@
+# ARGV is the argument variable
+# line below "unpacks" ARGV so that rather than holding all the args,
+# it's assigned to 3 variables
+first, second, third =  ARGV
+
+puts "Your first variable is: #{first}"
+puts "Your second variable is: #{second}"
+puts "Your third variable is: #{third}"
