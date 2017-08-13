a8eb53679cf73abcc3c505a782639010d1c6083b
mplibunao-shore <mark.libunao@shoresuite.com>
2017-08-13 17:09:19 +0800

    Added .gets.chomp

--- ex13.rb
+++ ex13.rb
@@ -3,6 +3,10 @@
 # it's assigned to 3 variables
 first, second, third =  ARGV
 
+print "What's your name? "
+name = $stdin.gets.chomp
+
+print "Hi #{name}, "
 puts "Your first variable is: #{first}"
 puts "Your second variable is: #{second}"
 puts "Your third variable is: #{third}"
