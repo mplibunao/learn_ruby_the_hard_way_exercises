499ae06d3e530c20e4ae7d766f6cdaf1f5240593
mplibunao-shore <mark.libunao@shoresuite.com>
2017-08-14 04:52:16 +0800

    Added exercise 18

---
+++ ex18.rb
@@ -0,0 +1,22 @@
+# *args is similar to ARGV / Spread operator
+def print_two(*args)
+  arg1, arg2 = args
+  puts "arg1: #{arg1}, arg2: #{arg2}"
+end
+
+def print_two_again(arg1, arg2)
+  puts "arg1: #{arg1}, arg2: #{arg2}"
+end
+
+def print_one(arg1)
+  puts "arg1: #{arg1}"
+end
+
+def print_none()
+  puts "I got nothin'."
+end
+
+print_two("Mp", "Libunao")
+print_two_again("Kyle", "Simpson")
+print_one("Zed")
+print_none()
